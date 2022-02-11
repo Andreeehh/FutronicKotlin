323a1654a8926c1bed8cd39d3ed6fc42c3379122
André Carvalho <andre.vrlcarvalho@hotmail.com>
2022-02-11 10:01:02 -0300

    Toggle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@
 import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.*
@@ -10,17 +11,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.ExperimentalKeyInput
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
-fun main() = Window(title = "Compose Biometry Desktop", size = IntSize(800,800)) {
+fun main() = Window(title = "Compose Biometry Desktop", size = IntSize(1200,1000)) {
     val darkTheme = savedInstanceState { true }
     MaterialTheme(colors = if (darkTheme.value) DarkGreenColorPalette else LightGreenColorPalette) {
         app(darkTheme)
@@ -34,22 +39,35 @@ fun app(darkTheme: MutableState<Boolean>) {
         Row {
                 Button(onClick = {
                 navItemState.value = NavType.HOME
-                },modifier = Modifier.offset(25.dp, 0.dp) )
+                },modifier = Modifier.offset(325.dp, 0.dp) )
                 {
                 Text("Biometria")
                 }
                 Button(onClick = {
                     navItemState.value = NavType.CARD
-                },modifier = Modifier.offset(25.dp, 0.dp) )
+                },modifier = Modifier.offset(425.dp, 0.dp) )
                 {
                     Text("Comandas")
                 }
                 Button(onClick = {
                     navItemState.value = NavType.HALLTABLE
-                },modifier = Modifier.offset(25.dp, 0.dp) )
+                },modifier = Modifier.offset(525.dp, 0.dp) )
                 {
                     Text("Mesa")
                 }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(width = 200.dp).offset(1000.dp, 0.dp).clickable { darkTheme.value = !darkTheme.value }.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Mudar Tema", style = MaterialTheme.typography.h6.copy(fontSize = 14.sp), color = MaterialTheme.colors.onSurface)
+            if (darkTheme.value) {
+                Icon(imageVector = Icons.Default.Star, tint = Color.Yellow)
+            } else {
+                Icon(imageVector = Icons.Default.Star, tint = MaterialTheme.colors.onSurface)
+            }
+
         }
         Row(modifier = Modifier.offset(0.dp,50.dp)) { bodyContent(navItemState.value) }
 
